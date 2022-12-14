e0a096adb426e6b856e1c5dee74697e409e109f2
Wheel <1462965949@qq.com>
2022-12-14 14:25:16 +0800

    Update dist.py

--- dist.py
+++ dist.py
@@ -3,24 +3,26 @@ import sys
 import subprocess
 
 
-with open("dll.txt", "w", encoding="utf-8") as f:
-    p = subprocess.Popen(["ldd", *sys.argv[1:]], stdout=f)
-    i, e = p.communicate()
-    p.terminate()
-    if e:
-        sys.exit(-1)
-
-
-with open("dll.txt", "r", encoding="utf-8") as f:
-    for line in f:
-        content = line.strip()
-        if not content:
-            continue
-        basename, path_addr = content.split(" => ")
-        path = " ".join(path_addr.split(" ")[:-1])
-        dirname = os.path.dirname(path)
-        if path.startswith("/c/"):
-            continue
-        p = subprocess.Popen(["cp", path, dirname])
-        p.communicate()
+for exe in sys.argv[1:]:
+    table = exe + "dll.txt"
+    with open(table, "w", encoding="utf-8") as f:
+        p = subprocess.Popen(["ldd", exe], stdout=f)
+        i, e = p.communicate()
         p.terminate()
+        if e:
+            sys.exit(-1)
+    with open(table, "r", encoding="utf-8") as f:
+        for line in f:
+            content = line.strip()
+            if not content:
+                continue
+            basename, path_addr = content.split(" => ")
+            path = " ".join(path_addr.split(" ")[:-1])
+            dirname = os.path.dirname(exe)
+            if path.startswith("/c/"):
+                continue
+            print("--COPY [%s]\n--TO [%s]\n" % (path, dirname))
+            p = subprocess.Popen(["cp", path, dirname])
+            p.communicate()
+            p.terminate()
+            print("--Complete")
